32883e66b2e813ecedfa3d777bbedbf05027ddd5
donmck <donmck@amazon.com>
2023-03-12 18:00:37 -0500

    Initial Commit

---
+++ iw2RunWalk Watch App/iw2RunWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  iw2RunWalkApp.swift
+//  iw2RunWalk Watch App
+//
+//  Created by Don McKenzie on 12-03-2023.
+//
+
+import SwiftUI
+
+@main
+struct iw2RunWalk_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iw2RunWalk/iw2RunWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  iw2RunWalkApp.swift
+//  iw2RunWalk
+//
+//  Created by Don McKenzie on 12-03-2023.
+//
+
+import SwiftUI
+
+@main
+struct iw2RunWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
